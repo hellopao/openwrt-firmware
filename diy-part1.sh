2afc07d4e8c20da8c8088ad2321cb3d64041c89f
泡泡 <qinjia@outlook.com>
2023-12-01 15:05:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git node https://github.com/nxhack/openwrt-node-packages.git' feeds.conf.default
